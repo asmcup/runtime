907475502b01103323336c6f3b3dc7a1c337a2bf
Kristopher Ives <kristopher.ives@gmail.com>
2016-10-15 04:43:06 -0700

    Cleaned up menu code.

--- src/asmcup/sandbox/Menu.java
+++ src/asmcup/sandbox/Menu.java
@@ -48,27 +48,27 @@ public class Menu extends JMenuBar {
 	
 	protected void addRobotMenu() {
 		JMenu menu = new JMenu("Robot");
-		menu.add(item("Teleport", (e) -> { teleport(); }));
+		menu.add(item("Teleport", e -> teleport()));
 		menu.addSeparator();
-		menu.add(item("Center View", (e) -> { centerView(); }));
+		menu.add(item("Center View", e -> centerView()));
 		menu.addSeparator();
-		menu.add(item("Show Code Editor", (e) -> { showCodeEditor(); }));
-		menu.add(item("Show Debugger", (e) -> { showDebugger(); }));
+		menu.add(item("Show Code Editor", e -> showCodeEditor()));
+		menu.add(item("Show Debugger", e -> showDebugger()));
 		add(menu);
 	}
 	
 	protected void addWorldMenu() {
 		JMenu menu = new JMenu("World");
-		menu.add(item("Generate New", (e) -> { reseed(); }));
+		menu.add(item("Generate New", e -> { reseed(); }));
 		menu.addSeparator();
-		menu.add(item("Load Snapshot...", (e) -> {  }));
+		menu.add(item("Load Snapshot...", e -> {  }));
 		menu.addSeparator();
-		menu.add(item("Save Snapshot", (e) -> {  }));
-		menu.add(item("Save Snapshot As...", (e) -> {  }));
+		menu.add(item("Save Snapshot", e -> {  }));
+		menu.add(item("Save Snapshot As...", e -> {  }));
 		menu.addSeparator();
-		menu.add(item("Show Info", (e) -> { showWorldInfo(); }));
+		menu.add(item("Show Info", e -> { showWorldInfo(); }));
 		menu.addSeparator();
-		menu.add(item("Quit", (e) -> { sandbox.quit(); }));
+		menu.add(item("Quit", e -> { sandbox.quit(); }));
 		add(menu);
 	}
 }
