df6b684f22cd9db7c5f0ce72767a6b68c9dd7dfc
Max Langhof <max.langhof@gmx.de>
2016-10-16 01:05:58 +0100

    Added shortcuts for main window.

--- src/asmcup/sandbox/Menu.java
+++ src/asmcup/sandbox/Menu.java
@@ -14,12 +14,18 @@ public class Menu extends JMenuBar {
 		addRobotMenu();
 	}
 	
-	public AbstractAction item(String name, ActionListener listener) {
-		return new AbstractAction(name) {
+	protected JMenuItem item(String label, ActionListener f,
+			KeyStroke shortcut) {
+		JMenuItem item = new JMenuItem();
+		item.setAction(new AbstractAction(label) {
 			public void actionPerformed(ActionEvent e) {
-				listener.actionPerformed(e);
+				f.actionPerformed(e);
 			}
-		};
+		});
+		if (shortcut != null) {
+			item.setAccelerator(shortcut);
+		}
+		return item;
 	}
 		
 	public void teleport() {
@@ -48,27 +54,32 @@ public class Menu extends JMenuBar {
 	
 	protected void addRobotMenu() {
 		JMenu menu = new JMenu("Robot");
-		menu.add(item("Teleport", e -> teleport()));
+		menu.add(item("Teleport", e -> teleport(),
+				KeyStroke.getKeyStroke(KeyEvent.VK_T, 0)));
 		menu.addSeparator();
-		menu.add(item("Center View", e -> centerView()));
+		menu.add(item("Center View", e -> centerView(),
+				KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0)));
 		menu.addSeparator();
-		menu.add(item("Show Code Editor", e -> showCodeEditor()));
-		menu.add(item("Show Debugger", e -> showDebugger()));
+		menu.add(item("Show Code Editor", e -> showCodeEditor(),
+				KeyStroke.getKeyStroke(KeyEvent.VK_E, 0)));
+		menu.add(item("Show Debugger", e -> showDebugger(),
+				KeyStroke.getKeyStroke(KeyEvent.VK_D, 0)));
 		add(menu);
 	}
 	
 	protected void addWorldMenu() {
 		JMenu menu = new JMenu("World");
-		menu.add(item("Generate New", e -> { reseed(); }));
+		menu.add(item("Generate New", e -> { reseed(); }, null));
 		menu.addSeparator();
-		menu.add(item("Load Snapshot...", e -> {  }));
+		menu.add(item("Load Snapshot...", e -> {  }, null));
 		menu.addSeparator();
-		menu.add(item("Save Snapshot", e -> {  }));
-		menu.add(item("Save Snapshot As...", e -> {  }));
+		menu.add(item("Save Snapshot", e -> {  }, null));
+		menu.add(item("Save Snapshot As...", e -> {  }, null));
 		menu.addSeparator();
-		menu.add(item("Show Info", e -> { showWorldInfo(); }));
+		menu.add(item("Show Info", e -> { showWorldInfo(); }, null));
 		menu.addSeparator();
-		menu.add(item("Quit", e -> { sandbox.quit(); }));
+		menu.add(item("Quit", e -> { sandbox.quit(); },
+				KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0)));
 		add(menu);
 	}
 }
