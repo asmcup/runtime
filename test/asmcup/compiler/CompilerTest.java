6b32382f1b6016dde736bfd3d9711ceaedbe5276
Tim Siebels <tim_siebels_aurich@yahoo.de>
2016-10-22 19:14:21 +0200

    test fail (#62)

--- test/asmcup/compiler/CompilerTest.java
+++ test/asmcup/compiler/CompilerTest.java
@@ -47,6 +47,7 @@ public class CompilerTest {
 
     @Test
     public void testCurrentLine() {
+        fail("TEST");
         try {
             compiler.compile("valid:\n\nundefinedfunction #0");
             fail("Compiler did not fail on undefined function.");
