856773ccb5933c919bf008e39ed5739f8deb32d5
Kristopher Ives <kristopher.ives@gmail.com>
2016-10-22 11:49:02 -0700

    Many improvements to genetics UI.

--- src/asmcup/sandbox/Genetics.java
+++ src/asmcup/sandbox/Genetics.java
@@ -13,6 +13,25 @@ public class Genetics extends JFrame {
 	protected final Sandbox sandbox;
 	protected int programSize = 256;
 	protected int fitnessFrames = 10 * 60;
+	protected Gene[] population = new Gene[100];
+	protected Random random = new Random();
+	protected Thread thread;
+	protected boolean running = false;
+	protected int generation = 0;
+	protected float startX, startY, startFacing;
+	protected int seed;
+	protected int mutationRate = 1;
+	protected int minMutationRate = 1;
+	protected int maxMutationRate = 100;
+	protected int controlCount = 0;
+	protected int chunkSize = programSize;
+	protected int idleMax = 0;
+	protected int exploreReward = 4;
+	protected int ramPenalty = 2;
+	protected int goldReward = 50;
+	protected int batteryReward = 100;
+	protected boolean temporal = true;
+	protected ArrayList<Gene> pinned = new ArrayList<>();
 	protected JLabel bestLabel = new JLabel("0");
 	protected JLabel worstLabel = new JLabel("0");
 	protected JLabel genLabel = new JLabel("0");
@@ -21,22 +40,22 @@ public class Genetics extends JFrame {
 	protected JButton startButton = new JButton("Start");
 	protected JButton stopButton = new JButton("Stop");
 	protected JButton saveButton = new JButton("Save");
+	protected JButton pinButton = new JButton("Pin");
+	protected JButton unpinButton = new JButton("Unpin");
+	protected ArrayList<JSpinner> spinners = new ArrayList<>();
 	protected JSpinner popSpinner = createSpinner(100, 1, 1000 * 1000);
 	protected JSpinner controlSpinner = createSpinner(0, 0, 100);
 	protected JSpinner mutationSpinner = createSpinner(100, 0, 100);
 	protected JSpinner sizeSpinner = createSpinner(256, 1, 256);
 	protected JSpinner frameSpinner = createSpinner(10 * 60, 1, 10 * 60 * 60 * 24);
-	protected Gene[] population = new Gene[100];
-	protected Random random = new Random();
-	protected Thread thread;
-	protected boolean running = false;
-	protected int generation = 0;
-	protected float startX, startY;
-	protected int seed;
-	protected int mutationRate = 1;
-	protected int minMutationRate = 1;
-	protected int maxMutationRate = 100;
-	protected int controlCount = 0;
+	protected JSpinner idleSpinner = createSpinner(0, 0, 1000 * 1000);
+	protected JSpinner chunkSpinner = createSpinner(chunkSize, 0, 256);
+	protected JSpinner exploreSpinner = createSpinner(exploreReward, -1000, 1000);
+	protected JSpinner rammingSpinner = createSpinner(ramPenalty, -1000, 1000);
+	protected JSpinner goldSpinner = createSpinner(goldReward, -1000, 1000);
+	protected JSpinner batterySpinner = createSpinner(batteryReward, -1000, 1000);
+	protected JSpinner temporalSpinner = createSpinner(temporal ? 1 : 0, 0, 1);
+	protected JPanel panel = new JPanel(new GridLayout(19, 2));
 	
 	public static class Gene implements Comparable<Gene> {
 		byte[] ram;
@@ -60,6 +79,10 @@ public class Genetics extends JFrame {
 	public Genetics(Sandbox sandbox) throws IOException {
 		this.sandbox = sandbox;
 		
+		setTitle("Genetics");
+		setResizable(false);
+		setIconImage(ImageIO.read(getClass().getResource("/dna.png")));
+		
 		for (int i=0; i < population.length; i++) {
 			population[i] = random();
 		}
@@ -68,53 +91,53 @@ public class Genetics extends JFrame {
 		startButton.addActionListener(e -> start());
 		stopButton.addActionListener(e -> stop());
 		saveButton.addActionListener(e -> save());
+		pinButton.addActionListener(e -> pin());
+		unpinButton.addActionListener(e -> unpin());
 		
-		JPanel panel = new JPanel(new GridLayout(11, 2));
+		hitem("Population:", popSpinner);
+		hitem("Frames:", frameSpinner);
+		hitem("Control:", controlSpinner);
+		hitem("Mutate Chance:", mutationSpinner);
+		hitem("Mutate Size:", chunkSpinner);
+		hitem("Size:", sizeSpinner);
+		hitem("Idle", idleSpinner);
+		hitem("Gold Reward", goldSpinner);
+		hitem("Battery Reward", batterySpinner);
+		hitem("Explore Reward", exploreSpinner);
+		hitem("Collide Reward", rammingSpinner);
+		hitem("Temporal:", temporalSpinner);
+		hitem("Best:", bestLabel);
+		hitem("Worst:", worstLabel);
+		hitem("Mutation:", mutationLabel);
+		hitem("Generation:", genLabel);
+		hitem(pinButton, unpinButton);
+		hitem(saveButton, flashButton);
+		hitem(stopButton, startButton);
 		
-		panel.add(new JLabel("Population:"));
-		panel.add(popSpinner);
-		
-		panel.add(new JLabel("Frames:"));
-		panel.add(frameSpinner);
-		
-		panel.add(new JLabel("Control:"));
-		panel.add(controlSpinner);
-		
-		panel.add(new JLabel("Max Mutate:"));
-		panel.add(mutationSpinner);
-		
-		panel.add(new JLabel("Size:"));
-		panel.add(sizeSpinner);
-		
-		panel.add(new JLabel("Best:"));
-		panel.add(bestLabel);
-		
-		panel.add(new JLabel("Worst:"));
-		panel.add(worstLabel);
-		
-		panel.add(new JLabel("Mutation:"));
-		panel.add(mutationLabel);
-		
-		panel.add(new JLabel("Generation:"));
-		panel.add(genLabel);
-		
-		panel.add(saveButton);
-		panel.add(flashButton);
-		
-		panel.add(stopButton);
-		panel.add(startButton);
-		
-		setTitle("Genetics");
-		setResizable(false);
-		setIconImage(ImageIO.read(getClass().getResource("/dna.png")));
 		setContentPane(panel);
 		pack();
+	}
+	
+	public void hitem(String label, JComponent component) {
+		hitem(new JLabel(label), component);
+	}
+	
+	public void hitem(JComponent a, JComponent b) {
+		panel.add(a);
+		panel.add(b);
 	}
 	
 	public JSpinner createSpinner(int value, int min, int max) {
 		SpinnerModel model = new SpinnerNumberModel(value, min, max, 1);
 		JSpinner spinner = new JSpinner(model);
+		spinners.add(spinner);
 		return spinner;
+	}
+	
+	public void setSpinnersEnabled(boolean enabled) {
+		for (JSpinner spinner : spinners) {
+			spinner.setEnabled(enabled);
+		}
 	}
 	
 	public int getInt(JSpinner spinner) {
@@ -126,21 +149,23 @@ public class Genetics extends JFrame {
 			return;
 		}
 		
-		popSpinner.setEnabled(false);
-		controlSpinner.setEnabled(false);
-		mutationSpinner.setEnabled(false);
-		sizeSpinner.setEnabled(false);
-		frameSpinner.setEnabled(false);
+		setSpinnersEnabled(false);
 		
 		controlCount = getInt(controlSpinner);
 		maxMutationRate = getInt(mutationSpinner);
 		programSize = getInt(sizeSpinner);
 		fitnessFrames = getInt(frameSpinner);
+		chunkSize = getInt(chunkSpinner);
+		idleMax = getInt(idleSpinner);
+		exploreReward = getInt(exploreSpinner);
+		ramPenalty = getInt(rammingSpinner);
+		temporal = getInt(temporalSpinner) > 0;
 		
 		resizePopulation();
 		
 		startX = sandbox.getRobot().getX();
 		startY = sandbox.getRobot().getY();
+		startFacing = sandbox.getRobot().getFacing();
 		seed = sandbox.getWorld().getSeed();
 		
 		thread = new Thread(new Runnable() {
@@ -179,12 +204,7 @@ public class Genetics extends JFrame {
 	
 	public void stop() {
 		running = false;
-		
-		popSpinner.setEnabled(true);
-		controlSpinner.setEnabled(true);
-		mutationSpinner.setEnabled(true);
-		sizeSpinner.setEnabled(true);
-		frameSpinner.setEnabled(true);
+		setSpinnersEnabled(true);
 	}
 	
 	public Gene random() {
@@ -213,11 +233,25 @@ public class Genetics extends JFrame {
 		}
 	}
 	
+	public void pin() {
+		pinned.add(getBest());
+	}
+	
+	public void unpin() {
+		pinned.clear();
+	}
+	
 	public void generation() {
 		int halfPoint = population.length / 2;
+		int pin = pinned.size();
 		
 		for (int i=halfPoint; i < population.length; i++) {
-			population[i] = cross();
+			if (pin > 0) {
+				pin--;
+				population[i] = cross(pinned.get(pin), randomGene());
+			} else {
+				population[i] = cross();
+			}
 		}
 		
 		Arrays.sort(population);
@@ -227,6 +261,11 @@ public class Genetics extends JFrame {
 		mutationRate = Math.min(maxMutationRate, mutationRate);
 		generation++;
 		updateStats();
+	}
+	
+	public Gene randomGene() {
+		int i = random.nextInt(population.length / 2);
+		return population[i];
 	}
 	
 	public Gene cross() {
@@ -249,7 +288,7 @@ public class Genetics extends JFrame {
 		
 		if (random.nextInt(100) <= mutationRate) {
 			dest = random.nextInt(programSize);
-			size = 1 + random.nextInt(programSize);
+			size = 1 + random.nextInt(chunkSize);
 			
 			for (int i=0; i < size; i++) {
 				gene.ram[(dest + i) % programSize] = (byte)random.nextInt(256);
@@ -294,12 +333,15 @@ public class Genetics extends JFrame {
 		}
 		
 		robot.position(x, y);
+		robot.setFacing(startFacing);
 		robot.flash(ram.clone());
 		
 		float score = 0.0f;
 		int lastGold = 0;
 		int lastBattery = robot.getBattery();
-		HashSet<Integer> explored = new HashSet<Integer>();
+		HashSet<Integer> explored = new HashSet<>();
+		HashSet<Integer> rammed = new HashSet<>();
+		int lastCol = 0, lastRow = 0, idle = 0;
 		
 		for (int frame=0; frame < fitnessFrames; frame++) {
 			world.tick();
@@ -308,33 +350,52 @@ public class Genetics extends JFrame {
 				break;
 			}
 			
-			if (robot.isRamming()) {
-				score -= 0.1f;
+			int collected = robot.getGold() - lastGold;
+			float t;
+			
+			if (temporal) {
+				t = 1.0f - (float)frame / (float)fitnessFrames;
+			} else {
+				t = 1.0f;
 			}
 			
-			int collected = robot.getGold() - lastGold;
-			float t = 1.0f - (float)frame / (float)fitnessFrames;
-			
 			if (collected > 0) {
-				score += t * 50;
+				score += t * goldReward;
 			}
 			
 			int recharged = robot.getBattery() - lastBattery;
 			
 			if (recharged > 0) {
-				score += t * 100;
+				score += t * batteryReward;
 			}
 			
-			int col = (int)(robot.getX() * 4.0f / World.TILE_SIZE);
-			int row = (int)(robot.getY() * 4.0f / World.TILE_SIZE);
+			int col = (int)(robot.getX() / World.TILE_SIZE);
+			int row = (int)(robot.getY() / World.TILE_SIZE);
 			int key = col | (row << 16);
 			
 			if (explored.add(key)) {
-				score += t * 5;
+				score += t * exploreReward;
+			}
+			
+			if (robot.isRamming() && rammed.add(key)) {
+				score -= t * ramPenalty;
 			}
 			
 			lastGold = robot.getGold();
 			lastBattery = robot.getBattery();
+			
+			if (frame > 0 && lastCol == col && lastRow == row) {
+				idle++;
+				
+				if (idleMax > 0 && idle > idleMax) {
+					break;
+				}
+			} else {
+				idle = 0;
+			}
+			
+			lastCol = col;
+			lastRow = row;
 		}
 		
 		return score;
