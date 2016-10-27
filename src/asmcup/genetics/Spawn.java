984611a511aab059c9c9e23753d11d6c01a2a3cd
Kristopher Ives <kristopher.ives@gmail.com>
2016-10-26 21:14:14 -0700

    Made Spawn class immutable.

--- src/asmcup/genetics/Spawn.java
+++ src/asmcup/genetics/Spawn.java
@@ -5,8 +5,8 @@ import java.util.Random;
 import asmcup.runtime.World;
 
 public class Spawn {
-	public float x, y, facing;
-	private int seed;
+	public final float x, y, facing;
+	public final int seed;
 	
 	public Spawn(float x, float y, float facing, int seed) {
 		this.x = x;
@@ -15,22 +15,21 @@ public class Spawn {
 		this.seed = seed;
 	}
 	
-	public Spawn applySeedOffset(int offset) {
-		Spawn spawn = new Spawn(x, y,
-				facing + (float)(offset * Math.PI * 0.25),
-				seed + offset);
+	public Spawn search(int offset) {
+		float sx = x, sy = y;
+		World world = getNewWorld();
+		Random random = new Random(seed + offset);
 		
-		World world = spawn.getNewWorld();
-		
-		Random random = new Random(offset);
 		// Wiggle around until the start position is fair.
 		// TODO ? Make this the job of world ("deterministic" random)?
-		while (world.isSolid(spawn.x, spawn.y, 25)) {
-			spawn.x += (random.nextFloat() - 0.5f) * World.CELL_SIZE;
-			spawn.y += (random.nextFloat() - 0.5f) * World.CELL_SIZE;
+		// TODO needs to check for an isSpawnable
+		
+		while (world.isSolid(sx, sy, 25)) {
+			sx += (random.nextFloat() - 0.5f) * World.CELL_SIZE;
+			sy += (random.nextFloat() - 0.5f) * World.CELL_SIZE;
 		}
 		
-		return spawn;
+		return new Spawn(sx, sy, facing, seed);
 	}
 	
 	public World getNewWorld() {
