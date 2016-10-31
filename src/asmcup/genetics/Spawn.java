a94922186d059ad74366bfb578a2701c7182bd55
Kristopher Ives <kristopher.ives@gmail.com>
2016-10-31 10:08:23 -0700

    Changed genetics to test bots in 45 degree incremenets. Changed genetics to give a small reward for surviving each frame.

--- src/asmcup/genetics/Spawn.java
+++ src/asmcup/genetics/Spawn.java
@@ -1,7 +1,5 @@
 package asmcup.genetics;
 
-import java.util.Random;
-
 import asmcup.runtime.World;
 
 public class Spawn {
@@ -13,23 +11,6 @@ public class Spawn {
 		this.y = y;
 		this.facing = facing;
 		this.seed = seed;
-	}
-	
-	public Spawn search(int offset) {
-		float sx = x, sy = y;
-		World world = getNewWorld();
-		Random random = new Random(seed + offset);
-		
-		// Wiggle around until the start position is fair.
-		// TODO ? Make this the job of world ("deterministic" random)?
-		// TODO needs to check for an isSpawnable
-		
-		while (world.isSolid(sx, sy, 25)) {
-			sx += (random.nextFloat() - 0.5f) * World.CELL_SIZE;
-			sy += (random.nextFloat() - 0.5f) * World.CELL_SIZE;
-		}
-		
-		return new Spawn(sx, sy, facing, seed);
 	}
 	
 	public World getNewWorld() {
