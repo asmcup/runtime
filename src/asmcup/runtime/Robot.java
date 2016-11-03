b17d4d91765312241252e167f8fd1bf12f79d2ab
Max Langhof <max.langhof@gmx.de>
2016-11-03 03:58:22 +0000

    Changed compass range. (#128)
    The compass now always returns a positive float between 0 and 2 pi.

--- src/asmcup/runtime/Robot.java
+++ src/asmcup/runtime/Robot.java
@@ -346,7 +346,7 @@ public class Robot {
 			vm.push8(world.recv(this, frequency));
 			break;
 		case IO_COMPASS:
-			vm.pushFloat(facing % (float)Math.PI);
+			vm.pushFloat(floatModPositive(facing, (float)(Math.PI * 2)));
 			break;
 		case IO_BEAM_DIRECTION:
 			beamDirection = popFloatSafe(-1.0f, 1.0f);
@@ -428,6 +428,10 @@ public class Robot {
 		}
 		
 		return f;
+	}
+	
+	protected static float floatModPositive(float dividend, float divisor) {
+		return ((dividend % divisor) + divisor) % divisor;
 	}
 	
 	public static final int IO_SENSOR = 0;
