f6605fe5e8391659695ab29c6d87dd07eba0dfa0
Max Langhof <max.langhof@gmx.de>
2016-11-05 22:36:35 +0000

    Added a dialog for loading a seed and bot location
    Additionally, fixed a bug where Sandbox.resetWorld(seed) would ignore the
    passed seed.

---
+++ src/asmcup/sandbox/LoadWorldDialog.java
@@ -0,0 +1,63 @@
+package asmcup.sandbox;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JSpinner;
+import javax.swing.BorderFactory;
+
+import asmcup.genetics.Spawn;
+import asmcup.runtime.World;
+
+public class LoadWorldDialog extends JFrame {
+	protected final Sandbox sandbox;
+	protected final FrontPanel panel = new FrontPanel();
+
+	protected JSpinner spinnerSeed; 
+	protected JSpinner spinnerX, spinnerY;
+	protected JSpinner spinnerFacing;
+	protected JButton loadButton = new JButton("Load");
+	protected JButton closeButton = new JButton("Close");
+	
+	public LoadWorldDialog(Sandbox sandbox) {
+		this.sandbox = sandbox;
+		
+		spinnerSeed   = panel.createSpinner(0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+		spinnerX      = panel.createSpinner(0.0f, 0f, (float)World.SIZE);
+		spinnerY      = panel.createSpinner(0.0f, 0f, (float)World.SIZE);
+		spinnerFacing = panel.createSpinner(0.0f, -(float)Math.PI, (float)Math.PI * 2, 0.1f);
+		panel.addRow("World Seed:", spinnerSeed);
+		panel.addRow("Robot X:", spinnerX);
+		panel.addRow("Robot Y:", spinnerY);
+		panel.addRow("Robot Facing:", spinnerFacing);
+		panel.setBorder(BorderFactory.createTitledBorder("Spawn Location"));
+		
+		loadButton.addActionListener(e -> load());
+		closeButton.addActionListener(e -> close());
+		panel.addItems(loadButton, closeButton);
+		
+		setTitle("Load World");
+		setResizable(false);
+		// TODO: Icon
+		setContentPane(panel);
+		pack();
+	}
+	
+	public void load() {
+		Spawn spawn = new Spawn(panel.getFloat(spinnerX),
+		                        panel.getFloat(spinnerY),
+		                        panel.getFloat(spinnerFacing),
+		                        panel.getInt(spinnerSeed));
+		sandbox.loadSpawn(spawn);
+	}
+	
+	public void update() {
+		spinnerSeed.setValue(sandbox.getWorld().getSeed());
+		spinnerX.setValue(sandbox.getRobot().getX());
+		spinnerY.setValue(sandbox.getRobot().getY());
+		spinnerFacing.setValue(sandbox.getRobot().getFacing());
+	}
+	
+	public void close() {
+		setVisible(false);
+	}
+}
