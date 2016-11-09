f98b21020c694a961f4a62a6674d66521b703a7a
Max Langhof <max.langhof@gmx.de>
2016-11-09 19:31:43 +0000

    Fixed gold and battery item sizes (literally).

--- src/asmcup/runtime/Generator.java
+++ src/asmcup/runtime/Generator.java
@@ -341,11 +341,11 @@ public class Generator {
 	}
 	
 	public Item.Gold gold() {
-		return new Item.Gold(1 + nextInt(99));
+		return new Item.Gold(50);
 	}
 	
 	public Item.Battery battery() {
-		return new Item.Battery(1 + nextInt(99));
+		return new Item.Battery(50);
 	}
 	
 	public static interface TileFunc {
